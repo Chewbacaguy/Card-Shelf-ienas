5410a095729aa1a3d59e087ce233be2d6b4b192a
Santiago Torres Alvarez <santiagot2000@gmail.com>
2023-10-26 15:06:03 +0200

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Santiago Torres Alvarez on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
